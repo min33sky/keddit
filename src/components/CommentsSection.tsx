8893ef42c87cc5370cde21d65e3b3d7891936736
min33sky <min33sky@naver.com>
2023-06-28 04:05:12 +0900

    feat: add reply to comment

--- src/components/CommentsSection.tsx
+++ src/components/CommentsSection.tsx
@@ -38,7 +38,7 @@ export default async function CommentsSection({
 
       <div className="flex flex-col gap-y-6 mt-4">
         {comments
-          .filter((comment) => !comment.replyToId)
+          .filter((comment) => !comment.replyToId) // Only top-level comments
           .map((topLevelComment) => {
             const topLevelCommentVotesAmt = topLevelComment.votes.reduce(
               (acc, vote) => {
@@ -63,6 +63,35 @@ export default async function CommentsSection({
                     postId={postId}
                   />
                 </div>
+
+                {/* 대댓글 랜더링 */}
+                {topLevelComment.replies
+                  .sort((a, b) => b.votes.length - a.votes.length) // Sort replies by most liked
+                  .map((reply) => {
+                    const replyVotesAmt = reply.votes.reduce((acc, vote) => {
+                      if (vote.type === 'UP') return acc + 1;
+                      if (vote.type === 'DOWN') return acc - 1;
+                      return acc;
+                    }, 0);
+
+                    const replyVote = reply.votes.find(
+                      (vote) => vote.userId === session?.user.id,
+                    );
+
+                    return (
+                      <div
+                        key={reply.id}
+                        className="ml-2 py-2 pl-4 border-l-2 border-zinc-200"
+                      >
+                        <PostComment
+                          comment={reply}
+                          currentVote={replyVote}
+                          votesAmt={replyVotesAmt}
+                          postId={postId}
+                        />
+                      </div>
+                    );
+                  })}
               </div>
             );
           })}
--- src/components/PostComment.tsx
+++ src/components/PostComment.tsx
@@ -1,10 +1,21 @@
 'use client';
 
 import type { Comment, CommentVote, User } from '@prisma/client';
-import React, { useRef } from 'react';
+import React, { startTransition, useRef, useState } from 'react';
 import UserAvatar from './UserAvatar';
 import formatDateString from '@/lib/formatDateString';
 import CommentVoteComponent from './CommentVote';
+import { Button } from './ui/Button';
+import { MessageSquare } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { useSession } from 'next-auth/react';
+import { Label } from './ui/Label';
+import { useOnClickOutside } from '@/hooks/use-on-click-outside';
+import { useMutation } from '@tanstack/react-query';
+import { CommentRequest } from '@/lib/validators/comment';
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
+import { Textarea } from './ui/Textarea';
 
 type ExtendedComment = Comment & {
   votes: CommentVote[];
@@ -25,6 +36,43 @@ export default function PostComment({
   postId,
 }: PostCommentProps) {
   const commentRef = useRef<HTMLDivElement>(null);
+  const router = useRouter();
+  const { data: session } = useSession();
+  const [isReplying, setIsReplying] = useState(false);
+  const [input, setInput] = useState<string>(`@${comment.author.username} `);
+  useOnClickOutside(commentRef, () => {
+    setIsReplying(false);
+  });
+
+  const { mutate: postComment, isLoading } = useMutation({
+    mutationFn: async ({ postId, text, replyToId }: CommentRequest) => {
+      const payload: CommentRequest = {
+        postId,
+        text,
+        replyToId,
+      };
+
+      const { data } = await axios.patch(
+        `/api/subreddit/post/comment`,
+        payload,
+      );
+      return data;
+    },
+
+    onError: (error) => {
+      toast.error('Something went wrong, please try again later');
+    },
+
+    onSuccess: () => {
+      startTransition(() => {
+        toast.success('Comment posted');
+        setIsReplying(false);
+        setInput('');
+        router.refresh();
+      });
+    },
+  });
+
   return (
     <div ref={commentRef} className="flex flex-col">
       <div className="flex items-center">
@@ -48,12 +96,71 @@ export default function PostComment({
 
       <p className="text-sm text-zinc-900 mt-2">{comment.text}</p>
 
-      <div className="flex gap-2 items-center">
+      <div className="flex gap-2 items-center flex-wrap">
         <CommentVoteComponent
           commentId={comment.id}
           votesAmt={votesAmt}
           currentVote={currentVote}
         />
+
+        <Button
+          onClick={() => {
+            if (!session) {
+              return router.push('/sign-in');
+            }
+            setIsReplying((prev) => !prev);
+          }}
+          variant={'ghost'}
+          size={'xs'}
+        >
+          <MessageSquare className="h-4 w-4 mr-1.5" />
+          답글
+        </Button>
+
+        {isReplying ? (
+          <div className="grid w-full gap-1.5">
+            <Label htmlFor="comment">댓글 달기</Label>
+            <div className="mt-2">
+              <Textarea
+                onFocus={(e) =>
+                  e.currentTarget.setSelectionRange(
+                    e.currentTarget.value.length,
+                    e.currentTarget.value.length,
+                  )
+                }
+                autoFocus
+                id="comment"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                rows={1}
+                placeholder="What are your thoughts?"
+              />
+
+              <div className="mt-2 flex justify-end gap-2">
+                <Button
+                  tabIndex={-1}
+                  variant="subtle"
+                  onClick={() => setIsReplying(false)}
+                >
+                  취소
+                </Button>
+                <Button
+                  isLoading={isLoading}
+                  onClick={() => {
+                    if (!input) return;
+                    postComment({
+                      postId,
+                      text: input,
+                      replyToId: comment.replyToId ?? comment.id, // default to top-level comment
+                    });
+                  }}
+                >
+                  등록
+                </Button>
+              </div>
+            </div>
+          </div>
+        ) : null}
       </div>
     </div>
   );
